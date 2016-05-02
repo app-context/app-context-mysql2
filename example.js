38c7ccb61642c9794bf09a927aec6a312728bddd
Matt Insler <matt.insler@gmail.com>
2016-05-02 13:56:34 -0400

    added new files and updated example

--- example.js
+++ example.js
@@ -1,23 +1,23 @@
 module.exports = function() {
   this.runlevel('connected')
     // attach a connection to APP.mysql - use the value at APP.config.mysql as the connection string
-    .use('mysql', '$mysql')
+    .use('mysql2', '$mysql')
 
     // attach a connection to APP.mysql
-    .use('mysql', 'mysql://localhost:3306/foobar')
+    .use('mysql2', 'mysql://localhost:3306/foobar')
 
     // attach a connection pool to APP.mysql
-    .use('mysql', {url: 'mysql://localhost:3306/foobar', pool: true})
+    .use('mysql2', {url: 'mysql://localhost:3306/foobar', pool: true})
 
     // create 2 connections and attach them as an object to APP.mysql
     // this will create APP.mysql.users and APP.mysql.data
-    .use('mysql', {
+    .use('mysql2', {
       users: '$mysql.users',
       data: '$mysql.data'
     })
 
     // you can also pass options to each connection
-    .use('mysql', {
+    .use('mysql2', {
       users: {url: 'mysql://localhost:3306/users', pool: true},
       data: {url: 'mysql://localhost:3306/data', connectTimeout: 3000}
     })
